d72370cef42a6b5a75ce7a4c0e15483c8f046b26
Shrivardhansinh Jadeja <163887472+22it044@users.noreply.github.com>
2024-10-03 16:03:13 +0530

    final it

---
+++ Todo/Models/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Todo.Database;
+
+namespace Todo.Models
+{
+    public class ApplicationDbContext: IdentityDbContext<User>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+            : base(options) { }
+
+    public DbSet<TodoItem> TodoItems { get; set; } = null!;
+    }
+
+}
